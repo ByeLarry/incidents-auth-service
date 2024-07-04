2fe40b7484524e178682a2ea9afa217fcc7e713e
Solovey Roman <gasoss221510@gmail.com>
2024-07-04 21:17:18 +0300

    chore

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,12 +4,28 @@ import { AppService } from './app.service';
 import { MongooseModule } from '@nestjs/mongoose';
 import { ConfigModule } from '@nestjs/config';
 import { UserModule } from './user/user.module';
+import { MailerModule } from '@nestjs-modules/mailer';
 
 @Module({
   imports: [
-    ConfigModule.forRoot(),
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
     MongooseModule.forRoot(`${process.env.MONGO_CONNECTION_STRING}`),
     UserModule,
+    MailerModule.forRoot({
+      transport: {
+        host: process.env.SMTP_HOST,
+        port: 465,
+        auth: {
+          user: process.env.SMTP_USER,
+          pass: process.env.SMTP_PASS,
+        },
+      },
+      defaults: {
+        from: process.env.SMTP_USER,
+      },
+    }),
   ],
   controllers: [AppController],
   providers: [AppService],
--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -5,24 +5,10 @@ import { MongooseModule } from '@nestjs/mongoose';
 import { User, UserSchema } from 'src/schemas/User.schema';
 import { Session, SessionSchema } from 'src/schemas/Session.schema';
 import { MailerModule } from '@nestjs-modules/mailer';
-import { ConfigModule } from '@nestjs/config';
 
 @Module({
   imports: [
-    ConfigModule.forRoot(),
-    MailerModule.forRoot({
-      transport: {
-        host: process.env.SMTP_HOST,
-        port: 465,
-        auth: {
-          user: process.env.SMTP_USER,
-          pass: process.env.SMTP_PASS,
-        },
-      },
-      defaults: {
-        from: process.env.SMTP_USER,
-      },
-    }),
+    MailerModule,
     MongooseModule.forFeature([
       { name: User.name, schema: UserSchema },
       { name: Session.name, schema: SessionSchema },
