5a041af65342926b043a4ce2092760f7d4904dab
Solovey Roman <gasoss221510@gmail.com>
2024-10-28 11:02:19 +0300

    refactor: update routing keys

--- src/libs/enums/index.ts
+++ src/libs/enums/index.ts
@@ -1,5 +1,5 @@
 export * from './date.enum';
-export * from './msg-auth.enum';
+export * from './message-auth.enum';
 export * from './roles.enum';
 export * from './auth-providers.enum';
 export * from './mongo-collections.enum';
