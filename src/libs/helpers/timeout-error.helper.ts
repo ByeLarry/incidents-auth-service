6f8764840a1eb2f279462c3cf9aa8e06dd6c95c8
Solovey Roman <gasoss221510@gmail.com>
2024-11-12 21:22:36 +0300

    feature: add handleTimeoutAndErrors

--- src/libs/helpers/timeout-error.helper.ts
+++ src/libs/helpers/timeout-error.helper.ts
@@ -1,16 +1,17 @@
-import { HttpStatus } from '@nestjs/common';
+import { HttpException, HttpStatus } from '@nestjs/common';
 import { Observable, TimeoutError } from 'rxjs';
 import { catchError, timeout } from 'rxjs/operators';
+import { TIMEOUT_ERROR_DELAY } from '../utils';
 
 export function handleTimeoutAndErrors<T = unknown>() {
   return (source$: Observable<T>) =>
     source$.pipe(
-      timeout(5000),
+      timeout(TIMEOUT_ERROR_DELAY),
       catchError((err) => {
         if (err instanceof TimeoutError) {
-          throw new Error(HttpStatus.REQUEST_TIMEOUT.toString());
+          throw new HttpException(err.message, HttpStatus.REQUEST_TIMEOUT);
         }
-        throw new Error(HttpStatus.BAD_REQUEST.toString());
+        throw new HttpException(err.message, HttpStatus.BAD_REQUEST);
       }),
     );
 }
